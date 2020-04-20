5958f7694c254a1afce05cf5fc9a8e9550e7ba90
Matt Weet <hello@mattweet.com>
2020-04-20 21:54:18 +0100

    :recycle: Move edit page info after ACF repeater

--- page.php
+++ page.php
@@ -21,19 +21,42 @@ get_header();
         <main id="main" class="site-main">
           <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <?php
-        while ( have_posts() ) :
-          the_post();
-
-          get_template_part( 'template-parts/content', 'page' );
-          get_template_part( 'template-parts/acf-flexible-content'); // Get flexible content from ACF
-
-          // If comments are open or we have at least one comment, load up the comment template.
-          if ( comments_open() || get_comments_number() ) :
-            comments_template();
-          endif;
-
-        endwhile; // End of the loop.
-        ?>
+              while ( have_posts() ) :
+                the_post();
+                get_template_part( 'template-parts/content', 'page' );
+                get_template_part( 'template-parts/acf-flexible-content'); // Get flexible content from ACF
+                wp_link_pages( array(
+                  'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jellypress' ),
+                  'after'  => '</div>',
+                ) );
+                // If comments are open or we have at least one comment, load up the comment template.
+                if ( comments_open() || get_comments_number() ) :
+                  comments_template();
+                endif;
+              endwhile; // End of the loop.
+            ?>
+            <?php if ( get_edit_post_link() ) : ?>
+              <footer class="entry-footer">
+                <?php
+                edit_post_link(
+                  sprintf(
+                    wp_kses(
+                      /* translators: %s: Name of current post. Only visible to screen readers */
+                      __( 'Edit <span class="screen-reader-text">%s</span>', 'jellypress' ),
+                      array(
+                        'span' => array(
+                          'class' => array(),
+                        ),
+                      )
+                    ),
+                    wp_kses_post( get_the_title() )
+                  ),
+                  '<span class="edit-link">',
+                  '</span>'
+                );
+                ?>
+              </footer><!-- .entry-footer -->
+            <?php endif; ?>
           </article><!-- #post-<?php the_ID(); ?> -->
         </main><!-- #main -->
       </div><!-- #primary -->
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,49 +9,20 @@
 
 ?>
 
-<section class="section section__intro">
-    <div class="row">
-      <div class="col">
+<section class="section">
+  <div class="row">
+    <div class="col">
       <header class="entry-header">
         <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
       </header><!-- .entry-header -->
 
       <?php jellypress_post_thumbnail();// TODO: Incorporate into theme
 ?>
-
       <div class="entry-content">
         <?php
         the_content();
-
-        wp_link_pages( array(
-          'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jellypress' ),
-          'after'  => '</div>',
-        ) );
         ?>
       </div><!-- .entry-content -->
-
-      <?php if ( get_edit_post_link() ) : ?>
-        <footer class="entry-footer">
-          <?php
-          edit_post_link(
-            sprintf(
-              wp_kses(
-                /* translators: %s: Name of current post. Only visible to screen readers */
-                __( 'Edit <span class="screen-reader-text">%s</span>', 'jellypress' ),
-                array(
-                  'span' => array(
-                    'class' => array(),
-                  ),
-                )
-              ),
-              wp_kses_post( get_the_title() )
-            ),
-            '<span class="edit-link">',
-            '</span>'
-          );
-          ?>
-        </footer><!-- .entry-footer -->
-      <?php endif; ?>
-      </div><!-- .col -->
-    </div><!-- .row -->
+    </div><!-- .col -->
+  </div><!-- .row -->
 </section><!-- .section__intro -->
