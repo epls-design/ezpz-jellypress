3324847aba2a053a8d6871aafed28f71ac9740cf
Matt Weet <hello@mattweet.com>
2020-06-29 18:13:35 +0100

    :pencil2: Comment consistency.

--- template-layouts/gallery.php
+++ template-layouts/gallery.php
@@ -7,25 +7,25 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 ?>
 
 <?php
   $section_id = get_query_var('section_id');
-  $image_ids = get_sub_field( 'images' );
+  $image_ids = get_sub_field('images');
   $size = 'medium';
   $columns = 3
 ?>
 
 <?php
-if ( $image_ids ) :
-// Generate string of ids ("123,456,789").
-$images_string = implode( ',', $image_ids );
-// Generate shortcode
-$gallery_shortcode = sprintf( '[gallery ids="%1$s" size="%2$s" columns="%3$s" link="none"]', $images_string, $size, $columns );
-?>
+if ($image_ids ) :
+    // Generate string of ids ("123,456,789").
+    $images_string = implode(',', $image_ids);
+    // Generate shortcode
+    $gallery_shortcode = sprintf('[gallery ids="%1$s" size="%2$s" columns="%3$s" link="none"]', $images_string, $size, $columns);
+    ?>
 <div class="row">
-  <?php echo do_shortcode( $gallery_shortcode ); ?>
+    <?php echo do_shortcode($gallery_shortcode); ?> <!-- TODO: Can this be styled better by default ? -->
 </div>
 <?php endif; ?>
