a4ed5c90c0de9eb5406ad77df0df4929e8ab1845
Matt Weet <hello@mattweet.com>
2020-08-25 21:35:16 +0100

    :construction: Continue upgrade from latest Jellyfish.

--- dist/js/site.js
+++ dist/js/site.js
@@ -52,6 +52,127 @@
 	}
 }() );
 ;
+//  TODO: Make this more DRY
+/**
+ * Function to allow the user to change the root font-size to increase legibility.
+ */
+jQuery('button.font-size').click(function () {
+  if ($(this).hasClass('font-size-sm')) {
+    $(document.body).removeClass('font-size-md font-size-lg').addClass('font-size-sm');
+    $('button.font-size').removeClass('active');
+    $(this).addClass('active');
+    jfCreateCookie('fontsize', 'sm');
+  }
+  else if ($(this).hasClass('font-size-md')) {
+    $(document.body).removeClass('font-size-sm font-size-lg').addClass('font-size-md');
+    $('button.font-size').removeClass('active');
+    $(this).addClass('active');
+    jfCreateCookie('fontsize', 'md');
+  }
+  else if ($(this).hasClass('font-size-lg')) {
+    $(document.body).removeClass('font-size-sm font-size-md').addClass('font-size-lg');
+    $('button.font-size').removeClass('active');
+    $(this).addClass('active');
+    jfCreateCookie('fontsize', 'lg');
+  };
+});
+
+/**
+ * Function to allow the user to change the contrast mode.
+ */
+jQuery('button.toggle-contrast').click(function () {
+  if ($(this).hasClass('active')) {
+    $(document.body).removeClass('dark-ui');
+    $(this).removeClass('active');
+    jfCreateCookie('ui-mode', 'light');
+  }
+  else {
+    $(document.body).addClass('dark-ui');
+    $(this).addClass('active');
+    jfCreateCookie('ui-mode', 'dark');
+  }
+});
+
+/**
+ * Check for Accessibility cookies 'fontsize' and 'ui-mode' on document ready. Append appropriate classes to body element
+ */
+jQuery(document).ready(function ($) {
+  var docFontSize = jfReadCookie('fontsize');
+  switch (docFontSize) {
+    case 'sm':
+      $(document.body).removeClass('font-size-md font-size-lg').addClass('font-size-sm');
+      $('button.font-size').removeClass('active');
+      $('button.font-size-sm').addClass('active');
+      break;
+    case 'md':
+      $(document.body).removeClass('font-size-sm font-size-lg').addClass('font-size-md');
+      $('button.font-size').removeClass('active');
+      $('button.font-size-md').addClass('active');
+      break;
+    case 'lg':
+      $(document.body).removeClass('font-size-md font-size-md').addClass('font-size-lg');
+      $('button.font-size').removeClass('active');
+      $('button.font-size-lg').addClass('active');
+      break;
+    default:
+      break;
+  };
+  var docUiMode = jfReadCookie('ui-mode');
+  switch (docUiMode) {
+    case 'dark' :
+      $(document.body).addClass('dark-ui');
+      $('button.toggle-contrast').addClass('active');
+      break;
+  }
+});
+
+;
+/**
+ * Creates a cookie
+ * @param {string} name
+ * @param {string} value
+ * @param {number} days before expiry (optional)
+ */
+function jfCreateCookie(name, value, days) {
+  var expires;
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = ";expires="+ date.toUTCString();
+  }
+  else {
+    expires = "";
+  }
+  // Sets the cookie site-wide with path=/
+  document.cookie = name + "=" + encodeURIComponent(value) + expires + ";path=/";
+}
+
+/**
+ * Reads and returns the value of a cookie
+ * @param {string} cookiename
+ */
+function jfReadCookie(cookiename) {
+  var name = cookiename + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+;
+function jfdebug() {
+  // Trigger debug mode by applying .jf-debug to document
+  var docBody = document.getElementsByTagName('body')[0];
+  docBody.classList.toggle('jf-debug');
+};
+;
 jQuery(document).ready(function ($) {
   var navPoint = '900'; // px value at which the navigation should change from a burger menu to inline list
 
@@ -63,12 +184,11 @@ jQuery(document).ready(function ($) {
     $(navmenu).slideToggle();
     // Toggle the state of the aria-expanded attribute for screen readers
     var menuItem = $(e.currentTarget);
-      if (menuItem.attr( 'aria-expanded') === 'true') {
-        $(this).attr( 'aria-expanded', 'false');
-      }
-      else {
-        $(this).attr( 'aria-expanded', 'true');
-      }
+    if (menuItem.attr('aria-expanded') === 'true') {
+      $(this).attr('aria-expanded', 'false');
+    } else {
+      $(this).attr('aria-expanded', 'true');
+    }
   });
 
   // If a menu item with children is clicked...
@@ -80,13 +200,7 @@ jQuery(document).ready(function ($) {
       // prevent the link from firing
       e.preventDefault();
       // add .drop-active class and aria-expanded to parent li
-      $(this).parent("li").toggleClass("drop-active").attr( 'aria-expanded', 'true');
+      $(this).parent("li").toggleClass("drop-active").attr('aria-expanded', 'true');
     }
   });
 });
-
-function jfdebug() {
-  // Trigger debug mode by applying .jf-debug to document
-  var docBody = document.getElementsByTagName('body')[0];
-  docBody.classList.toggle('jf-debug');
-}
