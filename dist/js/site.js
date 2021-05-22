d037d1a7860e2e50930ccb761e9a59ed44895b9d
Matt Weet <hello@mattweet.com>
2021-05-22 09:41:18 +0100

    Globally expose breakpoints in JS

--- dist/js/site.js
+++ dist/js/site.js
@@ -52,6 +52,15 @@
 	}
 }() );
 ;
+// Note: These should match the values set in scss. Currently it is a manual process to keep them in sync
+var breakPoints = {
+  navBar: 900,
+  sm: 600,
+  md: 900,
+  lg: 1200,
+  xl: 1800,
+};
+;
 /**
  * Function to allow the user to change the root font-size to increase legibility.
  */
@@ -265,35 +274,40 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 ;
 jQuery(document).ready(function ($) {
-  var navPoint = '900'; // px value at which the navigation should change from a burger menu to inline list
-
   // Expand and Collapse .navbar-menu when clicking .hamburger
-  $(".hamburger").on('click touch', function (e) {
+  $(".hamburger").on("click touch", function (e) {
     // Search the parent .navbar for the .navbar-menu and store as a variable
-    var navmenu = $(this).parents('.navbar').find('.navbar-menu');
+    var navmenu = $(this).parents(".navbar").find(".navbar-menu");
     // Slide the navmenu into view
     $(navmenu).slideToggle();
     // Toggle the state of the aria-expanded attribute for screen readers
     var menuItem = $(e.currentTarget);
-    if (menuItem.attr('aria-expanded') === 'true') {
-      $(this).attr('aria-expanded', 'false');
+    if (menuItem.attr("aria-expanded") === "true") {
+      $(this).attr("aria-expanded", "false");
     } else {
-      $(this).attr('aria-expanded', 'true');
+      $(this).attr("aria-expanded", "true");
     }
   });
 
   // If a menu item with children is clicked...
-  $(document).on("click", 'li.has-children > a:not(".clicked"), li.menu-item-has-children > a:not(".clicked")', function (e) {
-    // ...and the window width is smaller than the navPoint
-    if ($(window).width() < (navPoint)) {
-      // add .clicked class to the anchor element
-      ($(this).addClass("clicked"));
-      // prevent the link from firing
-      e.preventDefault();
-      // add .drop-active class and aria-expanded to parent li
-      $(this).parent("li").toggleClass("drop-active").attr('aria-expanded', 'true');
+  $(document).on(
+    "click",
+    'li.has-children > a:not(".clicked"), li.menu-item-has-children > a:not(".clicked")',
+    function (e) {
+      // ...and the window width is smaller than the breakPoints.navBar
+      if ($(window).width() < breakPoints.navBar) {
+        // add .clicked class to the anchor element
+        $(this).addClass("clicked");
+        // prevent the link from firing
+        e.preventDefault();
+        // add .drop-active class and aria-expanded to parent li
+        $(this)
+          .parent("li")
+          .toggleClass("drop-active")
+          .attr("aria-expanded", "true");
+      }
     }
-  });
+  );
 });
 ;
 (function($) {
