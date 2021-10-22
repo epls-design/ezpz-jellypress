c43f399f8f166c5d6a2d365a2c86cb28aee48930
Matt <hello@mattweet.com>
2021-10-22 17:28:51 +0100

    Added Stacks CPT and block (#62)
    * Registers CPT and assigns ACF
    
    * Hide from search engines and redirect to home if not admin
    
    * Adds ACF description
    
    * Create ACF view
    
    * Pass stack ID throguh with Block ID
    
    * Stops adding .first and .last to blocks as we don't really use them
    
    * CSS styling of tooltip
    
    * Ignore scss in linter as it throws errors
    
    * Linter rules path

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Theme functions and definitions
  * This file simply pulls in partials from /inc
@@ -9,15 +10,15 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if(file_exists( get_template_directory() . '/env.json' )) {
+if (file_exists(get_template_directory() . '/env.json')) {
   $vars = file_get_contents(get_template_directory() . '/env.json');
   $vars = json_decode($vars, true);
   foreach ($vars as $key => $value) putenv("$key=$value");
 }
 
-if(!function_exists('jellypress_env')) {
+if (!function_exists('jellypress_env')) {
   function jellypress_env($key, $default = null)
   {
     $value = getenv($key);
@@ -26,26 +27,26 @@ if(!function_exists('jellypress_env')) {
   }
 }
 
-if (! defined('DEV_URL') ) define('DEV_URL', jellypress_env('DEV_URL'));
-if (! defined('STAGING_URL') ) define('STAGING_URL', jellypress_env('STAGING_URL'));
-if (! defined('PROD_URL') ) define('PROD_URL', jellypress_env('PROD_URL'));
+if (!defined('DEV_URL')) define('DEV_URL', jellypress_env('DEV_URL'));
+if (!defined('STAGING_URL')) define('STAGING_URL', jellypress_env('STAGING_URL'));
+if (!defined('PROD_URL')) define('PROD_URL', jellypress_env('PROD_URL'));
 
 $jellypress_includes = array(
   'inc/tgm-plugin-activation.php',   // Third party script to allow required/recommended plugins
   'inc/plugins.php',                 // Require and recommend plugins for this theme
   'inc/helpers.php',                 // Useful helper functions
-	'inc/theme-setup.php',             // Basic theme setup
-	'inc/widgets.php',                 // Register widget areas
+  'inc/theme-setup.php',             // Basic theme setup
+  'inc/widgets.php',                 // Register widget areas
   'inc/enqueue.php',                 // Register and enqueue scripts and styles.
   'inc/editor.php',                  // Customise editor
   'inc/remove-comments.php',         // Include this to completely remove support for comments
   'inc/template-tags.php',           // Custom template tags for this theme.
   'inc/template-functions.php',      // Functions which enhance the theme by hooking into WordPress.
-	'inc/pagination.php',              // Custom pagination for this theme.
+  'inc/pagination.php',              // Custom pagination for this theme.
   'inc/acf.php',                     // Functions which hook into ACF to add additional functionality to the site.
   'inc/dry.php',                     // Don't repeat yourself! Functions which reduce repetition in the theme code.
   'inc/video-embed.php',             // Functions to help with embedding videos.
-	'inc/customizer.php',              // Customizer additions.
+  'inc/customizer.php',              // Customizer additions.
   'inc/ajax-loadmore/loadmore.php',  // Uses Wordpress AJAX to lazyload more posts.
   'inc/remote-images.php',           // Uses images from a remote production URL if working in the local dev environment.
   'inc/schema.php',                  // Hook into WP_Footer to print Structured Schema markup
@@ -58,18 +59,19 @@ $jellypress_includes = array(
   //~~~~~ CUSTOM POST TYPES
   'inc/post_types/post.php',
   'inc/post_types/page.php',
+  'inc/post_types/stacks.php',
 
   //~~~~~ USER CAPABILITIES
   //'inc/user_caps/client-admin.php',  // Example for how to restrict user capabilities for a specific role
 );
 
-foreach ( $jellypress_includes as $file ) {
+foreach ($jellypress_includes as $file) {
   $filepath = get_template_directory() . '/' . $file;
-  if(file_exists($filepath)) require_once $filepath;
+  if (file_exists($filepath)) require_once $filepath;
 }
 
-if ( class_exists( 'woocommerce' ) ) {
+if (class_exists('woocommerce')) {
   // Only include Woo Support if Woo Installed
   $woo_support = get_template_directory() . '/inc/woocommerce.php';
-  if(file_exists($woo_support)) require_once $woo_support;
+  if (file_exists($woo_support)) require_once $woo_support;
 };
