726a2ddc8ac237d79f977ed99bb4927897da4c6c
Matt <hello@mattweet.com>
2022-02-19 09:09:06 +0000

    SEO improvement: Remove outdated hentry from post_class

--- inc/schema.php
+++ inc/schema.php
@@ -1,14 +1,28 @@
 <?php
+
 /**
  * Hook Schema markup into WP Footer
  *
  * @package jellypress
  */
 
-add_action( 'wp_footer', 'jellypress_faq_schema', 100 );
-if ( ! function_exists( 'jellypress_faq_schema' ) ) :
-  function jellypress_faq_schema() {
+add_action('wp_footer', 'jellypress_faq_schema', 100);
+if (!function_exists('jellypress_faq_schema')) :
+  function jellypress_faq_schema()
+  {
     global $faq_schema;
-    if(!empty($faq_schema)) echo '<script type="application/ld+json">'. json_encode($faq_schema) .'</script>';
+    if (!empty($faq_schema)) echo '<script type="application/ld+json">' . json_encode($faq_schema) . '</script>';
+  }
+endif;
+
+/**
+ * Remove hentry from post_class
+ */
+add_filter('post_class', 'jellypress_remove_hentry_class');
+if (!function_exists('jellypress_remove_hentry_class')) :
+  function jellypress_remove_hentry_class($classes)
+  {
+    $classes = array_diff($classes, array('hentry'));
+    return $classes;
   }
 endif;
