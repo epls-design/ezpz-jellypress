cbbd25fbd6d8bb4acd90f29054df45ca7d4606a4
Matt Weet <matt@epls.design>
2022-07-15 15:56:50 +0100

    TODO Note

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -179,6 +179,7 @@ function jellypress_scripts_add_atts($tag, $handle, $src)
   // Anything to defer goes in this array...
   $defer_scripts = ['svg4everybody', 'search-filter-plugin-chosen', 'search-filter-plugin-build', 'jquery-ui-sortable', 'jquery-ui-resizable', 'wc-cart-fragments'];
 
+  // TODO: Make it so ACF is deferred on front end only (not admin) - or do these get wrapped in a not logged in function?
   if (in_array($handle, $defer_scripts)) {
     if (false === stripos($tag, 'defer')) {
       $tag = str_replace(' src', ' defer="defer" src', $tag);
