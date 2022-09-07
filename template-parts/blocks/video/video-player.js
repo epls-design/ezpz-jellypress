5f3c46ca4ca42ddf0bd20b7c0173d176b4fe65d0
Matt Weet <matt@epls.design>
2022-09-07 16:42:22 +0100

    Autoplaying videos

--- template-parts/blocks/video/video-player.js
+++ template-parts/blocks/video/video-player.js
@@ -10,6 +10,16 @@ function jellypressPlayVimeo() {
   //vimeoPlayer.play();
 }
 
+function jellypressStopVideo(videoContainer) {
+  // When a modal with class modal__video is closed, stop autoplay
+  var iframe = videoContainer.find("iframe"),
+    src = iframe.prop("src");
+  src = src.replace("&autoplay=1", "");
+  iframe.prop("src", src);
+}
+
+var playVideoScript;
+
 (function ($) {
   function jellypressPlayVideo(videoWrapper) {
     var $wrapper = videoWrapper,
@@ -21,48 +31,54 @@ function jellypressPlayVimeo() {
     // console.log('wrapper', $wrapper);
     // console.log('iframe', iframe);
 
-    $wrapper.addClass("playing"); // Fades out the overlay
+    if (!$wrapper.hasClass("has-modal")) {
+      $wrapper.addClass("playing"); // Fades out the overlay
 
-    if ($button.hasClass("platform-vimeo")) {
-      if (typeof $iframe.attr("src") === "undefined") {
-        $iframe.attr("src", $button.data("src")); // Insert the src from the button's data-attr
-        vimeoPlayer = new Vimeo.Player(iframe); // Create Vimeo Player
-        vimeoPlayer.on("loaded", jellypressPlayVimeo); // Play when loaded
-      } else {
-        // If the video already has a src, play it
-        vimeoPlayer = new Vimeo.Player(iframe);
-        jellypressPlayVimeo();
-      }
-      vimeoPlayer.play();
-    } else if ($button.hasClass("platform-youtube")) {
-      if (typeof $iframe.attr("src") === "undefined") {
-        $iframe.attr("src", $button.data("src")); // Insert the src from the button's data-attr
-
-        var youTubePlayer = new YT.Player(iframe, {
-          // Use the YouTube API to create a new player
-          events: {
-            onReady: jellypressPlayYoutube,
-            //"onError": function (e) {
-            //  console.log(e);
-            //}
-          },
-        });
-
-        // Fallback if the API doesn't work
-        if (typeof youTubePlayer.playVideo === "undefined") {
-          youTubePlayer = $iframe;
-          $iframe.attr(
-            "src",
-            $iframe.attr("src").replace("autoplay=0", "autoplay=1")
-          );
+      if ($button.hasClass("platform-vimeo")) {
+        if (typeof $iframe.attr("src") === "undefined") {
+          $iframe.attr("src", $button.data("src")); // Insert the src from the button's data-attr
+          vimeoPlayer = new Vimeo.Player(iframe); // Create Vimeo Player
+          vimeoPlayer.on("loaded", jellypressPlayVimeo); // Play when loaded
+        } else {
+          // If the video already has a src, play it
+          vimeoPlayer = new Vimeo.Player(iframe);
+          jellypressPlayVimeo();
         }
-      } else {
-        // If the video already has a src, play it
-        var youTubePlayer = new YT.Player(iframe);
-        jellypressPlayYoutube;
+        vimeoPlayer.play();
+      } else if ($button.hasClass("platform-youtube")) {
+        if (typeof $iframe.attr("src") === "undefined") {
+          $iframe.attr("src", $button.data("src")); // Insert the src from the button's data-attr
+
+          var youTubePlayer = new YT.Player(iframe, {
+            // Use the YouTube API to create a new player
+            events: {
+              onReady: jellypressPlayYoutube,
+              //"onError": function (e) {
+              //  console.log(e);
+              //}
+            },
+          });
+
+          // Fallback if the API doesn't work
+          if (typeof youTubePlayer.playVideo === "undefined") {
+            youTubePlayer = $iframe;
+            $iframe.attr(
+              "src",
+              $iframe.attr("src").replace("autoplay=0", "autoplay=1")
+            );
+          }
+        } else {
+          // If the video already has a src, play it
+          var youTubePlayer = new YT.Player(iframe);
+          jellypressPlayYoutube;
+        }
       }
+    } else {
+      $button.trigger("click");
     }
   }
+
+  playVideoScript = jellypressPlayVideo;
 
   $(document).on("click touch", ".video-wrapper", function () {
     jellypressPlayVideo($(this));
