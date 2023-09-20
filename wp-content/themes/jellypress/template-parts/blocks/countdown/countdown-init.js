7c57db90e1109ad5d837b71cf027098fdf332bf1
Matt Weet <matt@epls.design>
2023-09-20 23:38:22 +0100

    ezpz/countdown

---
+++ wp-content/themes/jellypress/template-parts/blocks/countdown/countdown-init.js
@@ -0,0 +1,49 @@
+(function ($) {
+  var countDownTotal,
+    countDownHours,
+    countDownMinutes,
+    countDownSeconds,
+    countDownEndtime,
+    countDownInterval;
+
+  function jfGetTimeRemaining(countDownEndtime) {
+    countDownTotal = Date.parse(countDownEndtime) - Date.parse(new Date());
+    countDownSeconds = Math.floor((countDownTotal / 1000) % 60);
+    countDownMinutes = Math.floor((countDownTotal / 1000 / 60) % 60);
+    countDownHours = Math.floor((countDownTotal / (1000 * 60 * 60)) % 24);
+    countDownDays = Math.floor(countDownTotal / (1000 * 60 * 60 * 24));
+
+    return {
+      countDownTotal,
+      countDownSeconds,
+      countDownMinutes,
+      countDownHours,
+      countDownDays,
+    };
+  }
+
+  function jfUpdateClock() {
+    const t = jfGetTimeRemaining(countDownEndtime);
+    daysSpan = countdownClock.querySelector(".days");
+    hoursSpan = countdownClock.querySelector(".hours");
+    minutesSpan = countdownClock.querySelector(".minutes");
+    secondsSpan = countdownClock.querySelector(".seconds");
+
+    if (t.countDownTotal >= 0) {
+      daysSpan.innerHTML = ("0" + t.countDownDays).slice(-2);
+      hoursSpan.innerHTML = ("0" + t.countDownHours).slice(-2);
+      minutesSpan.innerHTML = ("0" + t.countDownMinutes).slice(-2);
+      secondsSpan.innerHTML = ("0" + t.countDownSeconds).slice(-2);
+    }
+    if (t.countDownTotal <= 0) {
+      countdownClock.classList.add("complete");
+      clearInterval(countDownInterval);
+    }
+  }
+
+  let countdownClock = document.querySelector(".countdown");
+  if (countdownClock) {
+    countDownEndtime = countdownClock.dataset.countdownTo;
+    setInterval(jfUpdateClock, 1000);
+  }
+})(jQuery);
--- wp-content/themes/jellypress/template-parts/blocks/countdown/view.php
+++ wp-content/themes/jellypress/template-parts/blocks/countdown/view.php
@@ -4,7 +4,7 @@
  * Countdown Block Template.
  *
  * @param array $block The block settings and attributes.
- * @param string $content The block inner HTML (empty).
+ * @param string $content The block inner HTML
  * @param bool $is_preview True during backend preview render.
  * @param int $post_id The post ID the block is rendering content against.
  *        This is either the post ID currently being displayed inside a query loop,
@@ -23,82 +23,57 @@
 defined('ABSPATH') || exit;
 
 $block_attributes = jellypress_get_block_attributes($block);
-$fields = get_fields();
-$text_align = $block_attributes['text_align'];
+$allowed_blocks = jellypress_get_allowed_blocks();
+$block_template = jellypress_get_block_template();
 
+$fields = get_fields();
+
+$block_id = str_replace('block_', '', $block_attributes['block_id']);
+$text_align = $block_attributes['text_align'];
 $complete_text = $fields['complete_text'];
+
 ?>
 
 <section class="<?php echo $block_attributes['class']; ?>" <?php echo $block_attributes['anchor']; ?>>
   <div class="container">
 
-    <?php if ($fields['title']) { ?>
-    <header class="row justify-center block-title">
-      <div class="col md-10 lg-8">
-        <h2 class="<?php echo $text_align; ?>">
-          <?php echo wp_strip_all_tags($fields['title']); ?>
-        </h2>
-      </div>
-    </header>
-    <?php } ?>
-
-    <?php if ($fields['preamble']) { ?>
-    <div class="row justify-center block-preamble">
-      <div class="col md-10 lg-8 <?php echo $text_align; ?>">
-        <?php echo $fields['preamble']; ?>
-      </div>
-    </div>
-    <?php } ?>
-
-    <?php if (!empty($fields['buttons'])) : ?>
-    <div class="row justify-center">
-      <div class="col md-10 lg-8 text-center">
-        <?php
-          if ($text_align == 'text-center') jellypress_display_cta_buttons($fields['buttons'], $block_attributes['bg_color'], 'justify-center');
-          elseif ($text_align == 'text-right') jellypress_display_cta_buttons($fields['buttons'], $block_attributes['bg_color'], 'justify-end');
-          else jellypress_display_cta_buttons($fields['buttons'], $block_attributes['bg_color']);            ?>
-      </div>
-    </div>
+    <?php if ($content || $is_preview) : ?>
+      <header class="row justify-center">
+        <div class="col md-10 lg-8">
+          <InnerBlocks className="<?php echo $text_align; ?>" allowedBlocks=" <?php echo $allowed_blocks; ?>" template="<?php echo $block_template; ?>" />
+        </div>
+      </header>
     <?php endif; ?>
 
     <?php if ($countdown = $fields['countdown_to']) :
-      $countdown_id = 'countdown-' . $block_id;
       $countdown_tz = $fields['time_zone'];
       $countdown = $countdown . ' ' . $countdown_tz;
       // eg. 'November 4 2020 18:00:00 GMT+0000'
     ?>
-    <div class="row justify-center <?php echo $text_align; ?>">
-      <div class="col md-10 lg-8">
-        <div id="<?php echo $countdown_id; ?>" class="countdown<?php if ($complete_text) echo ' has-complete-text'; ?>">
-          <div class="partial">
-            <div class="value days">00</div>
-            <div class="small"><?php _e('Days', 'jellypress'); ?></div>
+      <div class="row justify-center <?php echo $text_align; ?>">
+        <div class="col md-10 lg-8">
+          <div class="countdown<?php if ($complete_text) echo ' has-complete-text'; ?>" data-countdown-to="<?php echo $countdown; ?>">
+            <div class="partial">
+              <div class="value days">00</div>
+              <div class="small"><?php _e('Days', 'jellypress'); ?></div>
+            </div>
+            <div class="partial">
+              <div class="value hours">00</div>
+              <div class="small"><?php _e('Hours', 'jellypress'); ?></div>
+            </div>
+            <div class="partial">
+              <div class="value minutes">00</div>
+              <div class="small"><?php _e('Minutes', 'jellypress'); ?></div>
+            </div>
+            <div class="partial">
+              <div class="value seconds">00</div>
+              <div class="small"><?php _e('Seconds', 'jellypress'); ?></div>
+            </div>
+            <?php if ($complete_text) echo '<div class="partial complete-text">' . $complete_text . '</div>'; ?>
           </div>
-          <div class="partial">
-            <div class="value hours">00</div>
-            <div class="small"><?php _e('Hours', 'jellypress'); ?></div>
-          </div>
-          <div class="partial">
-            <div class="value minutes">00</div>
-            <div class="small"><?php _e('Minutes', 'jellypress'); ?></div>
-          </div>
-          <div class="partial">
-            <div class="value seconds">00</div>
-            <div class="small"><?php _e('Seconds', 'jellypress'); ?></div>
-          </div>
-          <?php if ($complete_text) echo '<div class="partial complete-text">' . $complete_text . '</div>'; ?>
         </div>
       </div>
-    </div>
     <?php endif; ?>
 
   </div>
 </section>
-
-<?php if ($countdown) {
-  add_action(
-    'wp_footer',
-    jellypress_countdown_init($countdown_id, $countdown),
-    30
-  ); // 30 priority ensures it is placed below the enqueued scripts (priority 20)
-} ?>
